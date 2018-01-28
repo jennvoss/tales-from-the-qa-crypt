18dc13c0db95d765c1284f09b6d6b1ca2296d544
Voss, Jennifer (ELS-PHI) <j.voss@elsevier.com>
2018-01-28 16:55:00 -0500

    Route server to /api

--- db/server.js
+++ db/server.js
@@ -6,8 +6,15 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults()
 
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1'
+  })
+);
+
 server.use(middlewares)
 server.use(router)
+
 server.listen(3001, () => {
   console.log('JSON Server is running on port 3001')
 })
